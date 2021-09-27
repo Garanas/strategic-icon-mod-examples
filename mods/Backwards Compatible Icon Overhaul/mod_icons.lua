a3cb609321fb63f2e7b13d908d2b0c676bac0ae5
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-27 21:15:59 +0200

    Add backwards compatible icon mod for ASI icons

---
+++ mods/Backwards Compatible Icon Overhaul/mod_icons.lua
@@ -0,0 +1,46 @@
+
+-- we do not have any manual assignments
+IconAssignments = { }
+
+--- Entry point for scripted assignments. This function has a limited scope and any changes to the 
+-- units and / or the projectiles do not affect the game as they are deep-copied of the originals. The 
+-- functionality that is available are LOG, WARN, _ALERT, SPEW, repr and all table, mathand string 
+-- operations and last the tonumber and type functions.
+-- @param units All available unit blueprints.
+-- @param projectiles All available projectile blueprints.
+-- @param icons All the file names of the icons part of this mod.
+function ScriptedIconAssignments (units, projectiles, icons)
+
+    -- for performance
+    local StringFind = string.find
+
+    -- this is where we store the results
+    local IconAssignments = {}
+
+    -- loop over all the units
+    for id, bp in units do
+
+        -- retrieve the original strategic icon name
+        local strategicIconName = bp.StrategicIconName
+
+        -- this can be nil
+        if strategicIconName then 
+
+            -- check if we have the same identifier in our icon folder
+            local partOfIconsMod = false 
+            for k, icon in icons do 
+                if StringFind(icon, strategicIconName, 1) then 
+                    partOfIconsMod = true 
+                    break
+                end
+            end
+
+            -- if we do, use that instead
+            if partOfIconsMod then 
+                IconAssignments[id] = { BlueprintId = id, IconSet = bp.StrategicIconName }
+            end
+        end
+    end
+    
+    return IconAssignments
+end
---
+++ mods/Backwards Compatible Icon Overhaul/mod_info.lua
@@ -0,0 +1,18 @@
+name = "Backwards Compatible Icon Overhaul"
+uid = "backwards-compatible-icon-overhaul-01"
+version = 1
+
+description = "This is an example mod that uses the scripted approach to be backwards compatible with the previous approach to make icons for."
+author = "(Jip) Willem Wijnia"
+
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+
+-- icon is made by: <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+icon = "/mods/Backwards Compatible Icon Overhaul/icon.png"
+
+conflicts = {  }
+before = {  }
+after = { "25D57D85-7D84-27HT-A501-BR3WL4N0000891" }
