0659bacc1ab0f76fa958eb1fe68cda0b729a707e
Willem Bart Wijnia <w.b.wijnia@gmail.com>
2021-09-27 14:38:42 +0200

    Add template mods with icons and script by Balthazar

---
+++ mods/BrewLAN UI Strategic Icon Overhaul Large/mod_info.lua
@@ -0,0 +1,17 @@
+name = "BrewLAN UI: Strategic Icon Overhaul Large"
+uid = "brewlan-ui-strategic-icon-overhaul-large-01"
+version = 1
+
+description = "This is the large set.\r\nReassigns strategic icons based on physical properties of the units. All rights reserved to Sean 'Balthazar' Wheeldon."
+copyright = "© 2015–2021 Sean Wheeldon"
+author = "Balthazar"
+
+selectable = true
+enabled = true
+exclusive = false
+ui_only = true
+icon = "/mods/BrewLAN UI Strategic Icon Overhaul Large/icon.png"
+
+conflicts = {  }
+before = {  }
+after = { "25D57D85-7D84-27HT-A501-BR3WL4N0000891" }
